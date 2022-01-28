7399a7eb686b6fb392d4337b960aed85c26bfa15
Chiril Cugureanu <cugureanuchiril@gmail.com>
2022-01-28 17:07:49 +0200

    Lil UX touches

--- src/hooks/useImageChanger.ts
+++ src/hooks/useImageChanger.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { PLAYERS_IMAGES } from "../images";
 
 let imageIdx = 0;
+let changeDelay = 500;
 
 const useImageChanger = () => {
   const [image, setImage] = useState(PLAYERS_IMAGES[imageIdx]);
@@ -10,7 +11,7 @@ const useImageChanger = () => {
     const interval = setInterval(() => {
       imageIdx < PLAYERS_IMAGES.length - 1 ? imageIdx++ : (imageIdx = 0);
       setImage(PLAYERS_IMAGES[imageIdx]);
-    }, 200);
+    }, changeDelay);
 
     return () => clearInterval(interval);
   }, []);
